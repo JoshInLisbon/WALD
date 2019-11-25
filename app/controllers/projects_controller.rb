5e273317d50608a02514e3a0f4e47653aa1e0371
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-11-25 18:42:39 +0000

    code beauty

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,57 +46,55 @@ class ProjectsController < ApplicationController
       @commands = commands
   end
 
-
   private
+    def commands
+      @commands = []
+      @tables_arr.each do |table|
+        command = "rails g model #{table[:table_name].downcase[0..-2]} "
+          table[:columns].each do |column|
+          next if column[:column_name] == "id"
 
-  def commands
-    @commands = []
-    @tables_arr.each do |table|
-      command = "rails g model #{table[:table_name].downcase[0..-2]} "
-        table[:columns].each do |column|
-        next if column[:column_name] == "id"
+            column_name = column[:column_name]
+            data_type = column[:data_type]
 
-          column_name = column[:column_name]
-          data_type = column[:data_type]
+              command += " #{column_name}:#{data_type}"
 
-            command += " #{column_name}:#{data_type}"
+            if column[:fk] == true
+              column[:relations].each do |e|
+              relation_table_name = e[:relation_table].downcase[0..-2]
 
-          if column[:fk] == true
-            column[:relations].each do |e|
-            relation_table_name = e[:relation_table].downcase[0..-2]
-
-            command += " #{relation_table_name}:refrences"
+              command += " #{relation_table_name}:refrences"
+            end
           end
         end
+      @commands << command
       end
-    @commands << command
+
+      @commands
     end
 
-    @commands
-  end
-
-  def to_data_type(schema_data_type)
-    case schema_data_type.downcase
-    when "integer", "tinyint", "smallint", "mediumint", "int", "bigint", "single precision", "double precision"
-      :integer
-    when "decimal"
-      :decimal
-    when "char", "varchar", "varbinary"
-      :string
-    when "text"
-      :text
-    when "binary", "bit"
-      :binary
-    when "boolean"
-      :boolean
-    when "date", "year"
-      :date
-    when "time"
-      :time
-    when "timestamp", "timestamp w/ tz", "timestamp wo/ tz"
-      :timestamp
-    when "datetime"
-      :datetime
+    def to_data_type(schema_data_type)
+      case schema_data_type.downcase
+      when "integer", "tinyint", "smallint", "mediumint", "int", "bigint", "single precision", "double precision"
+        :integer
+      when "decimal"
+        :decimal
+      when "char", "varchar", "varbinary"
+        :string
+      when "text"
+        :text
+      when "binary", "bit"
+        :binary
+      when "boolean"
+        :boolean
+      when "date", "year"
+        :date
+      when "time"
+        :time
+      when "timestamp", "timestamp w/ tz", "timestamp wo/ tz"
+        :timestamp
+      when "datetime"
+        :datetime
+      end
     end
-  end
 end
