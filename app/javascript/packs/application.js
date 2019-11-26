eef71a98a5f07abf66b0171629163216a639f81b
Joshua Summers <J.Eichler-Summers@sms.ed.ac.uk>
2019-11-26 12:47:02 +0000

    styles v1

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,9 @@
 import "bootstrap";
+import { highterjs } from "../plugins/highlight"
+
+const projectShow = document.querySelector(".projects-show");
+if(projectShow) {
+  highterjs();
+}
+
+
---
+++ app/javascript/plugins/highlight.js
@@ -0,0 +1,11 @@
+import hljs from 'highlightjs';
+import 'highlightjs/styles/dark.css';
+
+const highterjs = () => {
+  hljs.initHighlightingOnLoad();
+  // options go here
+
+}
+
+
+export { highterjs }
