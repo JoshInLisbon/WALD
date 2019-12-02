b4a4cc508a50803506fcf6d725c0f2aac8dc1663
sapir alagem <sapir.alagem@gmail.com>
2019-12-02 13:48:29 +0000

    commands for github/heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import { copyToClipboard } from "../components/copy_to_clipboard"
 import { indexSearch } from "../components/index_search"
 
 import { deviseLink } from "../components/devise_link"
+import { githubLink } from "../components/devise_link"
+
 
 const projectShow = document.querySelector(".projects-show");
 if(projectShow) {
