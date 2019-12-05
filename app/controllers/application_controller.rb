c0b36440e4706ab0867dd76c2954e1d2f5975f1c
Pedro Cruz <pedro_cruz_1995@hotmail.com>
2019-12-05 11:11:18 +0000

    meta test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
-  
   before_action :authenticate_user!
+
+  def default_url_options
+  { host: ENV["http://www.waldproject.com/"] || "localhost:3000" }
+end
 end
